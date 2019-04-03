0ae1bce4126ba8b27ab03da6317549b7ab5a133d
Michael Moffitt <mikeymoff91@gmail.com>
2019-04-03 16:28:34 -0600

    Fix Deck Controller for devise

--- app/controllers/api/decks_controller.rb
+++ app/controllers/api/decks_controller.rb
@@ -1,32 +1,49 @@
 class Api::DecksController < ApplicationController
-    def index
-        render json: Deck.all
-      end
-    
-      def create
-        deck = Deck.new(deck_params)
-        if deck.save
-          render json: deck
-        else
-          render json: { errors: deck.errors }, status: :unprocessable_entity 
+        before_action :set_deck, only: [:show, :edit, :update]
+      
+        def index
+          @decks = current_user.decks
         end
+      
+        def show
+        end
+      
+        def new
+          @deck = current_user_decks.new
+        end
+      
+        def create
+          @deck = current_user.decks.new(deck_params)
+          if @deck.save
+            redirect_to decks_path
+          else
+            render :new
+          end
+        end
+      
+        def edit
+        end
+      
+        def update
+          if @deck.update(deck_params)
+            redirect_to decks_path
+          else
+            render :edit
+          end
+        end
+      
+        def destroy
+          @deck.destroy
+          redirect_to deck_path
+        end
+      
+        private
+          def deck_params
+            params.require(:deck).permit(:name)
+          end
+      
+          def set_account
+            @deck = current_user.decks.find(params[:id])
+          end
+      
       end
-    
-      def update
-        deck = Deck.find(params[:id])
-        deck.update(complete: !deck.complete)
-        render json: deck
-      end
-    
-      def destroy
-        Deck.find(params[:id]).destroy
-        render json: { message: 'Deck deleted' }
-      end
-    
-      private
-    
-      def deck_params
-        params.require(:deck).permit(:name, :complete)
-      end
-    
-end
